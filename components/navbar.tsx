279902547c860fa37f4fb017a8ac1be5e00c3a85
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-01-07 01:49:08 +0500

    added dark theme to the project

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,14 @@
 import { UserButton } from "@clerk/nextjs"
 
 import MobileSidebar from "@/components/mobile-sidebar"
+import { ModeToggle } from "@/components/mode-toggle"
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
         <MobileSidebar />
-        <div className="flex w-full justify-end">
+        <div className="flex w-full justify-end space-x-4 items-center">
+          <ModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
     </div>
